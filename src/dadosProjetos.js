81ba53e7ed3afdf768d037559e9c846f2d7f446f
Giovanni <giovannivalieri5@gmail.com>
2025-04-04 19:17:53 -0300

    Paragráfos adicionados e cor da estrela atualizada

--- src/dadosProjetos.js
+++ src/dadosProjetos.js
@@ -2,10 +2,9 @@ export default [
     {
         id: 1,
         nome: "DeméTech",
-        descricao: "Projeto da faculdade de A.D.S. que desenvolvi usando ASP.Net com arquitetura MVC." 
-        + " Este Projeto consiste em uma loja virtual que permite criar contas, logar nessas contas e adicionar e remover itens ao "
-        + "carrinho. O site foi hospedado na nuvem Azure, e possui conexão com uma base de dados SQL Server para armazenar os dados "
-        + "dos clientes e realizar buscar de alimentos disponíveis.",
+        descricao: `Projeto da faculdade de A.D.S. que desenvolvi usando ASP.Net com arquitetura MVC.
+        Este Projeto consiste em uma loja virtual que permite criar contas, logar nessas contas e adicionar e remover itens ao carrinho. 
+        O site foi hospedado na nuvem Azure, e possui conexão com uma base de dados SQL Server para armazenar os dados dos clientes e realizar buscar de alimentos disponíveis.`,
         imagem:{
             src: "src/assets/images/projetos/demetech.gif",
             alt: "Gif mostrando a loja virtual chamada DeméTech, onde é aprensentado o login de usuário e o carrinho de compras virtual."
@@ -15,11 +14,9 @@ export default [
     {
         id: 2,
         nome: "HttpClientApi",
-        descricao: "Este projeto consiste em uma API Rest que se comunica com outra API."
-        + "Este projeto foi desenvolvido usando ASP.Net C#. Nele, apliquei medidas de seguranças com chave API para autorizar a"
-        + " requisição realizada, e política CORS para definir quem pode realizar a requisição e quais tipos de requisições são permitidas."
-        + " Utilizei a classe HttpClient para realizar as requisições HTTP a outra API, permitindo definir o enderenço de conexão e os"
-        + " serviços que a minha API realiza e permite o usuário realizar.",
+        descricao: `Este projeto consiste em uma API Rest que se comunica com outra API.
+        O projeto foi desenvolvido usando ASP.Net C#. Nele, apliquei medidas de seguranças com chave API para autorizar a requisição realizada, e política CORS para definir quem pode realizar a requisição e quais tipos de requisições são permitidas.
+        Utilizei a classe HttpClient para realizar as requisições HTTP a outra API, permitindo definir o enderenço de conexão e os serviços que a minha API realiza e permite o usuário realizar.`,
         imagem:{
             src: "src/assets/images/projetos/httpclientapi.gif",
             alt: "Gif mostrando um pouco do projeto HttpClientApi, onde é realizado uma requisição http get para receber os dados de outra api."
@@ -29,8 +26,8 @@ export default [
     {
         id: 3,
         nome: "Viagens - React",
-        descricao: "Meu primeiro projeto desenvolvido usando a biblioteca React. Desenvolvi este projeto para colocar em pratica o que"
-        + " eu aprendi sobre componentes em React. Mesmo sendo algo simples, é essencial colocar em prática o que aprendi na teoria.",
+        descricao: `Meu primeiro projeto desenvolvido usando a biblioteca React. 
+        Desenvolvi este projeto para colocar em pratica o que eu aprendi sobre componentes em React. Mesmo sendo algo simples, é essencial colocar em prática o que aprendi na teoria.`,
         imagem:{
             src: "src/assets/images/projetos/viagens.gif",
             alt: "Gif mostrando o projeto Viagens, onde aparece três registros de viagens a diferentes lugares no mundo."
@@ -40,8 +37,8 @@ export default [
     {
         id: 4,
         nome: "Gerador hexadecimal",
-        descricao: "Este projeto consiste em um simples gerador hexadecimal desenvolvido usando Javascript."
-        + " O foco é praticar minha lógica de programação utilizando Javascript e desenvolver usando vetores, funções e loops.",
+        descricao: `Este projeto consiste em um simples gerador hexadecimal desenvolvido usando Javascript.
+        O foco é praticar minha lógica de programação utilizando Javascript e desenvolver usando vetores, funções e loops.`,
         imagem:{
             src: "src/assets/images/projetos/hexadecimal.gif",
             alt: "Gif mostrando o gerador hexadecimal, onde é mostrado cada cor sendo gerada ao clicar no botão gerar cor."
