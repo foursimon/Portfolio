de1a02edf416457800fac936b6bcce711a1c12de
Giovanni <giovannivalieri5@gmail.com>
2025-04-04 12:43:01 -0300

    Updates

---
+++ dadosHabilidades.js
@@ -0,0 +1,113 @@
+//Armazenar esse dados em uma base de dados para atualizar o portfólio de forma dinâmica
+
+export default [
+    {
+        id:1,
+        nome: "Css",
+        nivel: 3,
+        metade: false,
+        descricao: "Utilizo CSS em todos meus projetos front-end. Para aprendizado sobre css, não utilizo framework. No entanto, "
+        + "já aprendi e usei Bootstrap para o projeto da faculdade de A.D.S.",
+        imagem:{
+            src: "./src/assets/images/design/css.png",
+            alt:  "logo do css",
+        }
+    },
+    {
+        id:2,
+        nome: "Html",
+        nivel: 3,
+        metade: false,
+        descricao: "HTML foi meu primeiro passo para desenvolvimento de front-end. Além de ser capaz de estruturar um site, sempre estou"
+        + " atento na semântica dos meus sites.",
+        imagem:{
+            src: "./src/assets/images/design/html.png",
+            alt:  "logo do html",
+        }
+    },
+    {
+        id:3,
+        nome: "C-Sharp",
+        nivel: 3,
+        metade: false,
+        descricao: "CSharp é a linguagem de programação que mais tenho familiaridade, sendo a minha escolha principal para desenvolvimento"
+        + " de API Rest e aplicação de Orientação a Objetos. Esta linguagem foi o que usei para estudar Orientação a Objetos.",
+        imagem:{
+            src: "./src/assets/images/design/csharp.png",
+            alt:  "logo do csharp",
+        }
+    },
+    {
+        id:4,
+        nome: "GitHub",
+        nivel: 3,
+        metade: false,
+        descricao: "Todos meus projetos são armazenados e compartilhados através do GitHub. Ver meu progresso em desenvolvimento "
+        + "através dos meus projetos mostra que estou aprendendo bastante, mas que há muito caminho pela frente.",
+        imagem:{
+            src: "./assets/images/design/github.png",
+            alt:  "logo do GitHub",
+        }
+    },
+    {
+        id:5,
+        nome: "ASP.Net",
+        nivel: 3,
+        metade: true,
+        descricao: "Minhas APIs são desenvolvidas usando ASP.Net. Já apliquei autenticação JWT, chave API, Entity Framework, conexão "
+        + "entre APIs com HttpClient e serviços CRUD em base de dados Relacionais. Além disso, também utilizo em aplicações com arquitetura MVC.",
+        imagem:{
+            src: "./assets/images/design/aspnet.png",
+            alt:  "logo do visual studio",
+        }
+    },
+    {
+        id:6,
+        nome: "Javascript",
+        nivel: 3,
+        metade: true,
+        descricao: "Utilizo Javascript para aplicar lógicas no front-end, principalmente em manipulações do DOM em projetos que não uso " +
+        "frameworks ou biblciotecas.",
+        imagem:{
+            src: "./assets/images/design/javascript.png",
+            alt:  "logo do javascript",
+        }
+    },
+    {
+        id:7,
+        nome: "React",
+        nivel: 2,
+        metade: false,
+        descricao: "Estudo React para desenvolver aplicações web, sendo a biblioteca que utilizei para desenvolver este portfólio."
+        + " Possuo conhecimento em propriedades (props) para reutilização de componentes e estados (state) para interações dinâmicas "
+        + "com o usuário.",
+        imagem:{
+            src: "./assets/images/design/react.svg",
+            alt:  "logo do react",
+        }
+    },
+    {
+        id:8,
+        nome: "Git",
+        nivel: 2,
+        metade: false,
+        descricao: "Utilizo o terminal Git para atualizar meus projetos no GitHub e ter mais controle das atualizações que realizo.",
+        imagem:{
+            src: "./assets/images/design/git.png",
+            alt:  "logo do git",
+        }
+    },
+    {
+        id:9,
+        nome: "Node Js",
+        nivel: 1,
+        metade: false,
+        descricao: "Utilizo Node Js para realizar requisições HTTP para outras APIs, principalmente para as APIs que eu desenvolvo."
+        + "Escolhi Node Js para facilitar as implementações em back-end e manter o consumo de API mais seguro escondendo as chaves"
+        + " de Api.",
+        imagem:{
+            src: "./assets/images/design/nodejs.png",
+            alt:  "logo do Node Js",
+        }
+    },
+]
---
+++ dadosLinhaTempo.js
@@ -0,0 +1,59 @@
+//Utilizar uma base de dados real para adicionar mais itens à linha do tempo de forma dinâmica
+
+export default [
+    {
+        id: 1,
+        title: "2023",
+        subtitle: "Comecei A.D.S na Unip",
+        descricao: "Na data de 23 de janeiro de 2023, comecei a cursar Análise e Desenvolvimento de Sistemas na Unip.",
+        imagem: {
+            src: "./assets/images/trajetoria/unip.png",
+            alt: "Logo da Unip. O fundo é azul com o nome unip escrito em amarelo e as palavras universidade paulista escrita logo abaixo em branco"
+        },
+        esferaGrande: true
+    },
+    {
+        id: 2,
+        title: "Curso extracurricular",
+        subtitle: "HTML Web Developer",
+        descricao: "Em 19 de fevereiro de 2024, com carga horária de 21h, concluí o curso de Formação HTML Web Developer na Digital Innovation One.",
+        imagem: {
+            src: "./assets/images/trajetoria/DioHtml.png",
+            alt: "Certificado de conclusão Formação HTML Web Developer da Digital Innovation One."
+        },
+        esferaGrande: false
+    },
+    {
+        id: 3,
+        title: "Curso extracurricular",
+        subtitle: "CSS Web Developer",
+        descricao: "Em 09 de agosto de 2024, com carga horária de 43h, concluí o curso de Formação CSS Web Developer na Digital Innovation One.",
+        imagem: {
+            src: "./assets/images/trajetoria/DioCss.png",
+            alt: "Certificado de conclusão Formação CSS Web Developer da Digital Innovation One."
+        },
+        esferaGrande: false
+    },
+    {
+        id: 4,
+        title: "2024",
+        subtitle: "Terminei o último semestre de A.D.S.",
+        descricao: "Em 2024, no dia 31 dezembro, foi finalizado o último ano letivo de A.D.S., marcando um grande passo em minha jornada.",
+        imagem: {
+            src: "./assets/images/trajetoria/unip.png",
+            alt: "Logo da Unip. O fundo é azul com o nome unip escrito em amarelo e as palavras universidade paulista escrita logo abaixo em branco"
+        },
+        esferaGrande: true
+    },
+    {
+        id: 5,
+        title: "2025",
+        subtitle: "Formatura na UNIP",
+        descricao: "Na data de 28 de fevereiro de 2025, realizei minha formatura, definindo uma grande vitória em minha vida. Agora irei continuar caminhando em busca de mais uma conquista.",
+        imagem: {
+            src: "./assets/images/trajetoria/formado.jpg",
+            alt: "Foto do autor, Giovanni Alves Valieri, usando uma beca com cor preta de formatura e segurando o canudo com seu certificado de conclusão."
+        },
+        esferaGrande: true
+    },
+]
