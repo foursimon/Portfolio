f0efc8cacfc3df610b9ceb7cc75d81596e4dd58b
Giovanni <giovannivalieri5@gmail.com>
2025-04-09 17:53:14 -0300

    api conectada, loading adicionado

--- src/componentes/Projetos.jsx
+++ src/componentes/Projetos.jsx
@@ -1,21 +1,33 @@
-import dadosProjetos from "../dadosProjetos.js"
+import {BuscarRepositorios} from "../../api/dadosProjetos.js"
 import "../assets/css/projetos.css"
+import imagemPadrao from "../assets/images/design/404.png"
+import {useState, useEffect} from "react"
 export function Projetos(){
-    const projetos = dadosProjetos.map(dado => (
-        <section key={dado.id} className="projeto-section">
-                <p>{dado.descricao}</p>
-                <div className="projeto-container">
-                    <h3>{dado.nome}</h3>
-                    <div className="projeto-img-container">
-                        <a href={dado.link} target="_blank"><img src={dado.imagem.src} alt={dado.imagem.alt} /></a>
-                    </div>
-                </div>
-        </section>
-    ))
+    const [projetos, setProjetos] = useState([])
+    useEffect(() =>{
+        async function Buscar(){
+            const repositorios = await BuscarRepositorios()
+            const lista = repositorios.map(dado => (
+                <section key={dado.Id} className="projeto-section">
+                        <p>{dado.Description}</p>
+                        <div className="projeto-container">
+                            <h3>{dado.Name}</h3>
+                            <div className="projeto-img-container">
+                                <a href={dado.Html_url} target="_blank"><img src={dado.Imagem ? dado.Imagem : imagemPadrao} /></a>
+                            </div>
+                        </div>
+                </section>
+            ))
+            setProjetos(lista)
+        }
+        Buscar()
+    }, [])
+
     return (
         <article id="projetos" className="projeto-article">
             <h2 className="section-title">Meus projetos - destaques</h2>
-            {projetos}
+            
+            {projetos.length > 0 ? projetos : <div className="loader"></div>}
         </article>
     )
 }
