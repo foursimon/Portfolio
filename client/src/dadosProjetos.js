2c17bdbd20bc75ba810ce46e695be213875aef83
Giovanni <giovannivalieri5@gmail.com>
2025-04-10 23:36:30 -0300

    caminho de imagem atualizado

--- client/src/dadosProjetos.js
+++ client/src/dadosProjetos.js
@@ -5,7 +5,7 @@ export default [
         Description: `Projeto da faculdade de A.D.S. que desenvolvi usando ASP.Net com arquitetura MVC.
         Este Projeto consiste em uma loja virtual que permite criar contas, logar nessas contas e adicionar e remover itens ao carrinho. 
         O site foi hospedado na nuvem Azure, e possui conexão com uma base de dados SQL Server para armazenar os dados dos clientes e realizar buscar de alimentos disponíveis.`,
-        Imagem: "src/assets/images/projetos/demetech.gif",
+        Imagem: "assets/images/projetos/demetech.gif",
         Html_url: "https://github.com/foursimon/DemeTech-Web"
     },
     {
@@ -14,7 +14,7 @@ export default [
         Description: `Este projeto consiste em uma API Rest que se comunica com outra API.
         O projeto foi desenvolvido usando ASP.Net C#. Nele, apliquei medidas de seguranças com chave API para autorizar a requisição realizada, e política CORS para definir quem pode realizar a requisição e quais tipos de requisições são permitidas.
         Utilizei a classe HttpClient para realizar as requisições HTTP a outra API, permitindo definir o enderenço de conexão e os serviços que a minha API realiza e permite o usuário realizar.`,
-        Imagem:"src/assets/images/projetos/httpclientapi.gif",
+        Imagem:"assets/images/projetos/httpclientapi.gif",
         Html_url: "https://github.com/foursimon/HttpClientApi"
     },
     {
@@ -22,7 +22,7 @@ export default [
         Name: "Viagens - React",
         Description: `Meu primeiro projeto desenvolvido usando a biblioteca React. 
         Desenvolvi este projeto para colocar em pratica o que eu aprendi sobre componentes em React. Mesmo sendo algo simples, é essencial colocar em prática o que aprendi na teoria.`,
-        Imagem:"src/assets/images/projetos/viagens.gif",
+        Imagem:"assets/images/projetos/viagens.gif",
         Html_url: "https://github.com/foursimon/Viagens-React"
     },
     {
@@ -30,7 +30,7 @@ export default [
         Name: "Gerador hexadecimal",
         Description: `Este projeto consiste em um simples gerador hexadecimal desenvolvido usando Javascript.
         O foco é praticar minha lógica de programação utilizando Javascript e desenvolver usando vetores, funções e loops.`,
-        Imagem:"src/assets/images/projetos/hexadecimal.gif",
+        Imagem:"assets/images/projetos/hexadecimal.gif",
         Html_url: "https://github.com/foursimon/Gerador-Hexadecimal"
     },
 ]
