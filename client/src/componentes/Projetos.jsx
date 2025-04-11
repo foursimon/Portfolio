a883e99b9ac63d012c41615ad3f39d77bc4c3368
Giovanni <giovannivalieri5@gmail.com>
2025-04-10 23:17:13 -0300

    Atualização de emergência

--- client/src/componentes/Projetos.jsx
+++ client/src/componentes/Projetos.jsx
@@ -1,12 +1,14 @@
 import "../assets/css/projetos.css"
 import imagemPadrao from "../assets/images/design/404.png"
+import dadosProjetos from "../dadosProjetos"
 import {useState, useEffect} from "react"
 import axios from "axios"
 export function Projetos(){
     const [projetos, setProjetos] = useState([])
     async function buscarApi(){
-        const resposta = await axios.get("http://localhost:8080/api")
-        const lista = resposta.data.map(dado => (
+        try{
+            const resposta = await axios.get("https://localhost:32773/api/Github")
+            const lista = resposta.data.map(dado => (
             <section key={dado.Id} className="projeto-section">
                     <p>{dado.Description}</p>
                     <div className="projeto-container">
@@ -16,8 +18,24 @@ export function Projetos(){
                         </div>
                     </div>
             </section>
-        ))
-        setProjetos(lista)
+            ))
+            setProjetos(lista)  
+        }
+        catch(erro){
+            console.log("Não foi possível pegar os repositórios recentes no momento. Aqui está alguns repositórios em destaques:")
+            const lista = dadosProjetos.map(dado => (
+            <section key={dado.Id} className="projeto-section">
+                    <p>{dado.Description}</p>
+                    <div className="projeto-container">
+                        <h3>{dado.Name}</h3>
+                        <div className="projeto-img-container">
+                            <a href={dado.Html_url} target="_blank"><img src={dado.Imagem ? dado.Imagem : imagemPadrao} /></a>
+                        </div>
+                    </div>
+            </section>
+            ))
+            setProjetos(lista)
+        }
     }
     useEffect(() =>{
         buscarApi()
---
+++ client/src/dadosProjetos.js
@@ -0,0 +1,37 @@
+export default [
+    {
+        Id: 1,
+        Name: "DeméTech",
+        Description: `Projeto da faculdade de A.D.S. que desenvolvi usando ASP.Net com arquitetura MVC.
+        Este Projeto consiste em uma loja virtual que permite criar contas, logar nessas contas e adicionar e remover itens ao carrinho. 
+        O site foi hospedado na nuvem Azure, e possui conexão com uma base de dados SQL Server para armazenar os dados dos clientes e realizar buscar de alimentos disponíveis.`,
+        Imagem: "src/assets/images/projetos/demetech.gif",
+        Html_url: "https://github.com/foursimon/DemeTech-Web"
+    },
+    {
+        Id: 2,
+        Name: "HttpClientApi",
+        Description: `Este projeto consiste em uma API Rest que se comunica com outra API.
+        O projeto foi desenvolvido usando ASP.Net C#. Nele, apliquei medidas de seguranças com chave API para autorizar a requisição realizada, e política CORS para definir quem pode realizar a requisição e quais tipos de requisições são permitidas.
+        Utilizei a classe HttpClient para realizar as requisições HTTP a outra API, permitindo definir o enderenço de conexão e os serviços que a minha API realiza e permite o usuário realizar.`,
+        Imagem:"src/assets/images/projetos/httpclientapi.gif",
+        Html_url: "https://github.com/foursimon/HttpClientApi"
+    },
+    {
+        Id: 3,
+        Name: "Viagens - React",
+        Description: `Meu primeiro projeto desenvolvido usando a biblioteca React. 
+        Desenvolvi este projeto para colocar em pratica o que eu aprendi sobre componentes em React. Mesmo sendo algo simples, é essencial colocar em prática o que aprendi na teoria.`,
+        Imagem:"src/assets/images/projetos/viagens.gif",
+        Html_url: "https://github.com/foursimon/Viagens-React"
+    },
+    {
+        Id: 4,
+        Name: "Gerador hexadecimal",
+        Description: `Este projeto consiste em um simples gerador hexadecimal desenvolvido usando Javascript.
+        O foco é praticar minha lógica de programação utilizando Javascript e desenvolver usando vetores, funções e loops.`,
+        Imagem:"src/assets/images/projetos/hexadecimal.gif",
+        Html_url: "https://github.com/foursimon/Gerador-Hexadecimal"
+    },
+]
+
