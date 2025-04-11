67406222b5b807d1362b718ac7f5c38be73908a6
Giovanni <giovannivalieri5@gmail.com>
2025-04-10 23:28:24 -0300

    Conexão com api diferente estabelecida / callback de projetos

--- client/src/componentes/Projetos.jsx
+++ client/src/componentes/Projetos.jsx
@@ -1,28 +1,45 @@
 import "../assets/css/projetos.css"
 import imagemPadrao from "../assets/images/design/404.png"
+import dadosProjetos from "../dadosProjetos"
 import {useState, useEffect} from "react"
 import axios from "axios"
 export function Projetos(){
     const [projetos, setProjetos] = useState([])
     async function buscarApi(){
-        const opcoes = {
-            method: "GET",
-            url: "https://portfolio-tw88.onrender.com/api",
-            mode: "cors"
-        }
-        const resposta = await axios.request(opcoes)
-        const lista = resposta.data.map(dado => (
-            <section key={dado.Id} className="projeto-section">
-                    <p>{dado.Description}</p>
-                    <div className="projeto-container">
-                        <h3>{dado.Name}</h3>
-                        <div className="projeto-img-container">
-                            <a href={dado.Html_url} target="_blank"><img src={dado.Imagem ? dado.Imagem : imagemPadrao} /></a>
+        try{
+            const opcoes = {
+                method: "GET",
+                url: "https://githubapi2.onrender.com/api/Github",
+                mode: "cors"
+            }
+            const resposta = await axios.request(opcoes)
+            const lista = resposta.data.map(dado => (
+                <section key={dado.Id} className="projeto-section">
+                        <p>{dado.Description}</p>
+                        <div className="projeto-container">
+                            <h3>{dado.Name}</h3>
+                            <div className="projeto-img-container">
+                                <a href={dado.Html_url} target="_blank"><img src={dado.Imagem ? dado.Imagem : imagemPadrao} /></a>
+                            </div>
                         </div>
-                    </div>
-            </section>
-        ))
-        setProjetos(lista)
+                </section>
+            ))
+            setProjetos(lista)
+        }catch (erro){
+            console.log("Não foi possível pegar os repositórios recentes no momento. Aqui está alguns repositórios em destaques:")
+            const lista = dadosProjetos.map(dado => (
+                <section key={dado.Id} className="projeto-section">
+                        <p>{dado.Description}</p>
+                        <div className="projeto-container">
+                            <h3>{dado.Name}</h3>
+                            <div className="projeto-img-container">
+                                <a href={dado.Html_url} target="_blank"><img src={dado.Imagem ? dado.Imagem : imagemPadrao} /></a>
+                            </div>
+                        </div>
+                </section>
+            ))
+            setProjetos(lista)
+        }
     }
     useEffect(() =>{
         buscarApi()
